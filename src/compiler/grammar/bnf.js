5bd543a88f095ff7d7b7f1b77104c6210cb646ae
cloudle <lehaoson@gmail.com>
2019-05-12 22:18:52 +0700

    pattern helper for bnf

--- src/compiler/grammar/bnf.js
+++ src/compiler/grammar/bnf.js
@@ -1,24 +1,33 @@
+class GeassAst {
+	constructor() {
+
+	}
+}
+
+function p(patternString, action, options) {
+	return [patternString, action, options];
+}
+
 export default {
 	expressions: [["e EOF",   "return $1"]],
 
 	e :[
-		["e + e",					"$$ = $1+$3"],
-		["e - e",					"$$ = $1-$3"],
-		["e * e",					"$$ = $1*$3"],
-		["e / e",					"$$ = $1/$3"],
-		["e ^ e",					"$$ = Math.pow($1, $3)"],
-		["e !",						"$$ = (function(n) {if(n==0) return 1; return arguments.callee(n-1) * n})($1)"],
-		["e %",						"$$ = $1/100"],
-		["- e",						"$$ = -$2", { prec: 'UMINUS' }],
-		["( e )",					"$$ = $2"],
-		["NUMBER",				"$$ = Number(yytext)"],
-		["E",							"$$ = Math.E"],
-		["PI",						"$$ = Math.PI"],
-		["IF",						"$$ = 2"],
-		["AND",						"$$ = 1"],
-		["OR",						"$$ = 1.5"],
-		["EQUAL",					"$$ = 5"],
-		["UNEQUAL",				"$$ = 10"],
-		["FUNCTION",			"$$ = 1"],
+		p('e + e', '$$ = $1+$3'),
+		p('e - e', '$$ = $1-$3'),
+		p('e * e', '$$ = $1*$3'),
+		p('e / e', '$$ = $1/$3'),
+		p('e ^ e', '$$ = Math.pow($1, $3)'),
+		p('e !', '$$ = (function(n) {if(n==0) return 1; return arguments.callee(n-1) * n})($1)'),
+		p('e %', '$$ = $1/100'),
+		p('- e', '$$ = -$2', { prec: 'UMINUS' }),
+		p('( e )', '$$ = $2'),
+		p('NUMBER', '$$ = Number(yytext)'),
+		p('E', '$$ = Math.E'),
+		p('PI', '$$ = Math.PI'),
+		p('IF', '$$ = 2'),
+		p('AND', '$$ = 1'),
+		p('EQUAL', '$$ = 5'),
+		p('UNEQUAL', '$$ = 10'),
+		p('FUNCTION', '$$ = 1'),
 	]
 };
